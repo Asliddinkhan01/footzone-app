f0192c9fff6e073a08ff3391cfd8d827966ee3b8
Asliddinkhan01 <asliddinkenjaev17@gmail.com>
2022-07-28 17:03:50 +0500

    first commit

---
+++ src/main/java/com/footzone/footzone/entity/footballSession/SessionDto2.java
@@ -0,0 +1,30 @@
+package com.footzone.footzone.entity.footballSession;
+
+
+//Asliddin Kenjaev, created: Jun, 23 2022 11:09 AM 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SessionDto2 {
+
+    private String sessionId;
+
+    private String startDate;
+
+    private String startTime;
+
+    private String endTime;
+
+    private String stadiumName;
+
+    private boolean stadiumHolder;
+
+    private UUID stadiumId;
+}
---
+++ src/test/java/com/footzone/footzone/HerokuFootzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footzone.footzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuFootzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
